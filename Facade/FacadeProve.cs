7d7fc5783b4a142df77512fc5e476ad9fc415174
vicky <10132231+fighting2014@user.noreply.gitee.com>
2022-09-05 08:18:04 +0800

    Facade 外观模式

---
+++ Facade/FacadeProve.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    /// <summary>
+    /// 外观模式类
+    /// </summary>
+    public class FacadeProve
+    {
+        private SubFlow1 subFlow1;
+        private SubFlow2 subFlow2;
+        private SubFlow3 subFlow3;
+        public FacadeProve()
+        {
+            subFlow1 = new SubFlow1();
+            subFlow2 = new SubFlow2();
+            subFlow3 = new SubFlow3();
+        }
+
+        //提供一个证明方法即可
+        public bool Prove()
+        {
+            return subFlow1.IsOk() && subFlow2.IsTrue() && subFlow3.IsGoodMan();
+        }
+    }
+
+    public class SubFlow1
+    {
+       public bool IsOk() => true;
+    }
+
+    public class SubFlow2
+    {
+        public bool IsTrue() => true;
+    }
+
+    public class SubFlow3
+    {
+        public bool IsGoodMan() => true;
+    }
+
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Facade;
+
+Console.WriteLine("Hello, World!");
+
+
+//假设 需要证明自己的身份需要  分别取流程 1  ，2  ，3  个部门分别开具证明
+//使用外观模式  只要跑一次就可以
+//外观模式缺点 违反开闭原则
+//优点 隐藏系统的复杂性 提供单一简便的调用
+
+
+new FacadeProve().Prove();
