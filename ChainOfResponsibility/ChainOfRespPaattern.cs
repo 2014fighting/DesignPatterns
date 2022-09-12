3a321e5a688be89d449bfb5eac3df13b51028343
vicky <10132231+fighting2014@user.noreply.gitee.com>
2022-09-12 23:19:11 +0800

    责任链

---
+++ ChainOfResponsibility/ChainOfRespPaattern.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public class ChainOfRespPaattern
+    {
+        /// <summary>
+        /// 请假方法
+        /// </summary>
+        public void Leave(int day)
+        {
+            var level1 = new Leader();
+            var level2 = new Boss();
+
+            level1.SetNextHandler(level2);
+
+            level1.Process(day);
+        }
+
+    }
+
+    /// <summary>
+    /// 先抽象一个处理类
+    /// </summary>
+    public abstract class Handler
+    {
+        protected Handler? _nextHandler;
+        public void SetNextHandler(Handler nextHandler)
+        {
+            this._nextHandler = nextHandler;
+        }
+
+        public abstract void Process(int info);
+    }
+
+    //实现处理类
+    public class Leader : Handler
+    {
+        public override void Process(int info)
+        {
+
+            if (_nextHandler == null)
+            {
+                //logrecode
+                return;
+            }
+            if (info > 0 && info < 11)
+            {
+                Console.WriteLine($"请假天数{info}天，经理 处理完成审批！");
+            }
+            else
+                _nextHandler.Process(info);
+
+        }
+    }
+
+    //实现处理类
+    public class Boss : Handler
+    {
+        public override void Process(int info)
+        {
+
+            Console.WriteLine($"请假天数{info}天，老板 处理完成审批！");
+
+        }
+    }
+
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ChainOfResponsibility;
+
+Console.WriteLine("Hello, World!");
+
+//责任链模式  就是将处理事务抽象 然后分不同的类来处理，通过指定下一个处理者将这些实现类串成一条链
+//优点是将请求和处理分开，请求者不需要知道谁去处理，其次可以提高处理灵活性，需要新增处理直接新增一个实现类即可
+//缺点每个请求都需要从链头到链尾，比较长是性能下降
+
+//请假三天
+new ChainOfRespPaattern().Leave(3);
+
+//请假13天
+new ChainOfRespPaattern().Leave(13);
